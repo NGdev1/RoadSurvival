bfae95df13e3fc1a0ab28352125e1fbaee571dd1
notantony <not.antony@yandex.ru>
2017-03-11 20:20:22 +0300

    PlayerMove changed

--- AnimalRun/Assets/Scripts/PlayerMove.cs
+++ AnimalRun/Assets/Scripts/PlayerMove.cs
@@ -3,59 +3,105 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-	enum playerState{
+	private enum playerState{
 		onEvent,
-		up,
-		down,
+		inAir,
 		idle,
-		right,
-		left
+		turn
 	};
-
-	private GameObject player;
-	private float jumpSpeed=0.5f, rotationSpeed=0.2f,runningSpeed=0.2f;
-	private playerState state;
-	// Use this for initialization
-	void Start () {
-		player = gameObject;
-		state = playerState.idle;
+	Vector3 x = new Vector3 (1, 0, 0);//nice to make them const
+	Vector3 rx = new Vector3 (-1, 0, 0);
+	Vector3 z = new Vector3 (0, 0, 1);
+	Vector3 rz = new Vector3 (0, 0, -1);
+	private Vector3 incAxis(Vector3 a){//next axis clockwise
+		if(a == x)
+			return rz;
+		if (a == rz)
+			return rx;
+		if(a == rx)
+			return z;
+		if(a == z)
+			return x;
+		print ("warning: strange axis in PlayerMove.incAxis");
+		return a;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey (KeyCode.RightArrow))
-			state = playerState.right;
-		if (Input.GetKey (KeyCode.LeftArrow))
-			state = playerState.left;
-		if (Input.GetKey (KeyCode.DownArrow))
-			state = playerState.idle;
-		transform.Translate (-runningSpeed,0,0);
-		switch (state) {
-		case playerState.idle:
-			if (Input.GetKey (KeyCode.RightArrow))
-				state = playerState.right;
-			if (Input.GetKey (KeyCode.LeftArrow))
-				state = playerState.left;
-			break;
-		case playerState.right:
-			transform.Translate (0, 0, rotationSpeed, player.transform);
-			break;
-		case playerState.left:
-			transform.Translate (0, 0, -rotationSpeed, player.transform);
-			break;
+
+	private Vector3 decAxis(Vector3 a){//next axis counterclockwise
+		if(a == rz)
+			return x;
+		if(a == rx)
+			return rz;
+		if(a == z)
+			return rx;
+		if(a == x)
+			return z;
+		print ("warning: strange axis in PlayerMove.decAxis");
+		return a;
+	}
+
+	private float turningSpeed = 0.2f,runningSpeed = 0.2f;
+	private playerState state;
+	public Vector3 mainAxis;
+	Vector3 delta;//current vector of movement
+
+	private int lineNumber;
+	private float[] spawnLines;
+
+	private void rotateRight(){//90 degree rotation
+		mainAxis=incAxis(mainAxis);
+		updateDelta ();
+	}
+	private void rotateLeft(){//90 degree rotation
+		mainAxis=decAxis(mainAxis);
+		updateDelta ();
+	}
+
+	private void updateDelta(){
+		delta = mainAxis * runningSpeed;
+	}
+
+	private void turnRight(){
+		if (lineNumber < spawnLines.Length - 1) {
+			delta += incAxis (mainAxis) * turningSpeed;
+			state = playerState.turn;
+			lineNumber++;
+		} else {
+			//TODO
 		}
 	}
-	void OnCollisionEnter(Collision col){
-		print ("hui");
-		state = playerState.idle;	
+
+	private void turnLeft(){
+		if (lineNumber > 0) {
+			delta += decAxis (mainAxis) * turningSpeed;
+			state = playerState.turn;
+			lineNumber--;
+		} else {
+			//TODO
+		}
 	}
-	void OnCollisionStay(Collision col){
-		print ("hui");
-		state = playerState.idle;	
-	} 
-	void OnTriggerEnter(Collider col){
-		print ("hui");
+
+	void Start () {
 		state = playerState.idle;
+		mainAxis = new Vector3(1,0,0);
+		delta.x = 0;delta.y = 0;delta.z = 0;
+		lineNumber = 1;
+		//TODO spawnLines = (float[])gameObject.FindObjectOfType (typeof(float[]));
+		spawnLines = new float[]{0.5f,1f,2f,3f};
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (delta);
+		switch (state) {
+		case playerState.idle:
+			if (Input.GetKey (KeyCode.UpArrow))//TMP 
+				updateDelta ();
+			if (Input.GetKey (KeyCode.RightArrow))
+				turnRight ();
+			if (Input.GetKey (KeyCode.LeftArrow))
+				turnLeft ();
+			break;
+		}
 	}
 
 }
